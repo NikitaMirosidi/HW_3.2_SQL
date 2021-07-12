5db8fd1be61f65666497530d63d137e8cdcf3b3e
Nikita <dr_mario@ua.fm>
2021-07-12 18:12:58 +0300

    second

---
+++ 2.sql
@@ -0,0 +1,9 @@
+create view costs as
+select homework_3.`dev+projects`.project_id, homework_3.projects.`name`, sum(homework_3.developers.salary) as sum
+from homework_3.`dev+projects`, homework_3.projects, homework_3.developers
+where homework_3.`dev+projects`.project_id = homework_3.projects.id
+and homework_3.`dev+projects`.developer_id = homework_3.developers.id
+and homework_3.developers.id in (select developer_id from homework_3.`dev+projects` where project_id = homework_3.projects.id)
+group by homework_3.`dev+projects`.project_id;
+
+select project_id, `name`, max(sum) from homework_3.costs;
