304509c4fccc5b020cf8f52181f867d0cb6b7db7
Nikita <dr_mario@ua.fm>
2021-07-13 10:51:35 +0300

    fourth

--- 2.sql
+++ 2.sql
@@ -1,24 +1,26 @@
-CREATE VIEW `costs` AS
-    SELECT 
-        `homework_3`.`dev+projects`.`project_id`,
-        `homework_3`.`projects`.`name`,
-        SUM(`homework_3`.`developers`.`salary`) AS `sum`
-    FROM
-        `homework_3`.`dev+projects`,
-        `homework_3`.`projects`,
-        `homework_3`.`developers`
-    WHERE
-        `homework_3`.`dev+projects`.`project_id` = `homework_3`.`projects.id`
-            AND `homework_3`.`dev+projects`.`developer_id` = `homework_3`.`developers.id`
-            AND `homework_3`.`developers.id` IN (SELECT 
-                `developer_id`
-            FROM
-                `homework_3`.`dev+projects`
-            WHERE
-                `project_id` = `homework_3`.`projects.id`)
-    GROUP BY `homework_3`.`dev+projects`.`project_id`;
-
-SELECT 
-    `project_id`, `name`, MAX(`sum`)
-FROM
-    `homework_3`.`costs`;
+SELECT MAX(`sum`)
+FROM (
+	SELECT SUM(`homework_3`.`developers`.`salary`) AS `sum`
+    FROM `homework_3`.`dev+projects`,
+		 `homework_3`.`projects`,
+         `homework_3`.`developers`
+    WHERE `homework_3`.`dev+projects`.`project_id` = `homework_3`.`projects`.`id`
+	AND `homework_3`.`dev+projects`.`developer_id` = `homework_3`.`developers`.`id`
+	AND `homework_3`.`developers`.`id` IN (
+		SELECT `developer_id`
+		FROM `homework_3`.`dev+projects`
+		WHERE `project_id` = `homework_3`.`projects`.`id`)
+    GROUP BY `homework_3`.`dev+projects`.`project_id`)
+	AS `table`;
+    
+SELECT MAX(`sum`)
+FROM (
+	SELECT `homework_3`.`dev+projects`.`project_id`,
+		   `homework_3`.`dev+projects`.`developer_id`,
+           `homework_3`.`developers`.`id`,
+		   SUM(`homework_3`.`developers`.`salary`) AS `sum`
+    FROM `homework_3`.`dev+projects`
+    JOIN `homework_3`.`developers`
+    ON `homework_3`.`dev+projects`.`developer_id` = `homework_3`.`developers`.`id`
+    GROUP BY `homework_3`.`dev+projects`.`project_id`)
+	AS `table`;
